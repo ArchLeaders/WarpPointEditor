53121e13bd3c1d1d6be717febaa806566f8e3e43
ArchLeaders <archparliament@gmail.com>
2023-01-22 22:08:18 -0800

    Add menu functions

--- WarpPointEditor/Models/ShellMenuModel.cs
+++ WarpPointEditor/Models/ShellMenuModel.cs
@@ -1,16 +1,75 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Generics.Dialogs;
 using Avalonia.MenuFactory.Attributes;
 using Material.Icons;
+using System.Diagnostics;
+using WarpPointEditor.Core.Extensions;
 using WarpPointEditor.ViewModels;
-using WarpPointEditor.Views;
 
 namespace WarpPointEditor.Models;
 
 public class ShellMenuModel
 {
+    //
+    // File
+
+    [Menu("Open Mod", "_File", Icon = MaterialIconKind.FolderOpen, HotKey = "Ctrl + O")]
+    public static void OpenMod()
+    {
+
+    }
+
+    [Menu("New Mod", "_File", Icon = MaterialIconKind.CreateNewFolder, HotKey = "Ctrl + N")]
+    public static void NewMod()
+    {
+
+    }
+
+    [Menu("Save Mod", "_File", Icon = MaterialIconKind.FloppyDisc, HotKey = "Ctrl + S", IsSeparator = true)]
+    public static void SaveMod()
+    {
+
+    }
+
+    [Menu("Quit", "_File", Icon = MaterialIconKind.ExitToApp, IsSeparator = true)]
+    public static async Task Quit()
+    {
+        if (DockFactory.Root?.VisibleDockables?.Count > 1) {
+            if (await MessageBox.ShowDialog("You may have unsaved changes. Are you sure you wish to exit?", "Warning", DialogButtons.YesNo) != DialogResult.Yes) {
+                return;
+            }
+        }
+
+        Environment.Exit(0);
+    }
+
+    //
+    // Tools
+
     [Menu("Settings", "_Tools", Icon = MaterialIconKind.CogBox)]
     public static void Settings()
     {
         DockFactory.AddDocument(new SettingsViewModel());
+    }
+
+    // 
+    // About
+
+    [Menu("Wiki", "_About", Icon = MaterialIconKind.HelpOutline)]
+    public static async Task Help()
+    {
+        await BrowserExtension.OpenUrl("https://github.com/ArchLeaders/WarpPointEditor/wiki");
+    }
+
+    [Menu("Discord", "_About", Icon = MaterialIconKind.LinkVariant)]
+    public static async Task Discord()
+    {
+        await BrowserExtension.OpenUrl("https://discord.gg/8Saj6tTkNB");
+    }
+
+    [Menu("Report Issue", "_About", Icon = MaterialIconKind.Bug, IsSeparator = true)]
+    public static async Task ReportIssue()
+    {
+        await BrowserExtension.OpenUrl("https://github.com/ArchLeaders/WarpPointEditor/issues/new");
     }
 }
