c6a17753cddfbcebc5d2ac6e0ec9bbee1ebb00de
ArchLeaders <archparliament@gmail.com>
2023-01-22 22:08:06 -0800

    Cleanup namespcaes

--- WarpPointEditor/ViewModels/ShellViewModel.cs
+++ WarpPointEditor/ViewModels/ShellViewModel.cs
@@ -1,8 +1,6 @@
 ﻿#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-using Avalonia.Controls;
 using Dock.Model.Core;
-using WarpPointEditor.Views;
 
 namespace WarpPointEditor.ViewModels;
 
